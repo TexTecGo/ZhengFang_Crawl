ad9a3865b79b943db154a70d935fd21d70cb8509
susmote <admin@susmote.com>
2018-05-18 19:27:42 +0800

    提交新的代码

--- get_homelink.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on 2018/5/18 
-
-@author: susmote
-"""
--- main.py
+++ main.py
@@ -5,62 +5,83 @@ Created on 2018/5/18
 @author: susmote
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on 2018/5/18 
-
-@author: susmote
-"""
 import requests
 from bs4 import BeautifulSoup
 from PIL import Image
-import time
+from get_operate_link import get_operate_link
+from crawl_func import get_timetable_dic,get_student_info
 
-headers={
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-    'Referer' : 'http://125.221.35.100/default2.aspx',
-    'Host' : '125.221.35.100'
-}
-url='http://125.221.35.100/default2.aspx'
-session=requests.session()
-soup=BeautifulSoup(session.get(url,headers=headers).text,'lxml')
-__VIEWSTATE=soup.find_all('input',type="hidden")[0]['value']
-number=input('输入账号：')
-passwd=input('输入密码：')
-t = str(int(time.time()*1000))
-captcha_url='http://125.221.35.100/CheckCode.aspx'
-r=session.get(captcha_url,headers=headers)
-print(r)
-with open('captcha.jpg','wb') as f:
-    f.write(r.content)
-im=Image.open('captcha.jpg')
-im.show()
-im.close()
-checkcode = input("请输入验证码 ： ")
+if __name__ == "__main__":
 
-postdata={
-    "__VIEWSTATE" : __VIEWSTATE,
-    "TextBox1": number,
-    "TextBox2": passwd,
-    "TextBox3": checkcode,
-    "Button1": "",
-    'RadioButtonList1': '\xd1\xa7\xc9\xfa',
-    'Button1': '',
-}
+    #全局变量
+    host = "125.221.35.100"  # 学校教务管理系统ip地址，可以改成你们学校的
 
-print(postdata)
-res=session.post('http://125.221.35.100/default2.aspx',data=postdata,headers=headers)
-
-#
-print(res.text)
-r = session.get('http://125.221.35.100/xs_main.aspx?xh=2017030548',headers=headers) #如果模拟登录成功，这里返回的是你教务系统的主页源代码
-classContent = session.get('http://125.221.35.100/xskbcx.aspx?xh=2017030548&xm=李斯特&gnmkdm=N121603',headers=headers)
+    url='http://'+host + "/" + 'default2.aspx'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+        'Referer': url,
+        'Host': host
+    }
+    session=requests.session()
 
 
+    # 获取标识码
+    soup=BeautifulSoup(session.get(url,headers=headers).text,'lxml')
+    __VIEWSTATE=soup.find_all('input',type="hidden")[0]['value']
 
+    # 输入需要的信息 学号，密码，验证码
+    studentid = input('输入学号：')
+    passwd = input('输入密码：')
+    captcha_url = 'http://'+host + "/" + 'CheckCode.aspx'
+    r=session.get(captcha_url,headers=headers)
+    print(r)
+    with open('captcha.jpg','wb') as f:
+        f.write(r.content)
+    im=Image.open('captcha.jpg')
+    im.show()
+    im.close()
+    checkcode = input("请输入验证码 ： ")
 
-print(r.text)
-print('学生课表',classContent.text)
+    # 封装需要post的数据
+    postdata={
+        "__VIEWSTATE": __VIEWSTATE,
+        "TextBox1": studentid,
+        "TextBox2": passwd,
+        "TextBox3": checkcode,
+        "Button1": "",
+        'RadioButtonList1': '\xd1\xa7\xc9\xfa',
+        'Button1': '',
+    }
 
-UserinfoContent = session.get('http://125.221.35.100/xsgrxx.aspx?xh=2017030548&xm=李斯特&gnmkdm=N121501',headers=headers)
-print('学生个人信息',UserinfoContent.text)
+    res = session.post(url, data=postdata, headers=headers)
+
+    if '验证码不正确' in res.text:
+        print("你输入的验证码不正确，请重新登录")
+        exit()
+
+    # 登录成功后，返回的是你教务系统的主页源代码
+    r = session.get('http://'+host + "/" + 'xs_main.aspx?xh='+ studentid,headers=headers)
+
+    link_dic = get_operate_link(host, r.text)
+    print(link_dic)
+
+    # 获取学生个人课表
+    student_class = session.get(link_dic['学生个人课表'], headers=headers)
+    print(get_student_info(student_class.text))
+    print(get_timetable_dic(student_class.text))
+
+    # 查询个人成绩
+    student_grade_query = session.get(link_dic['成绩查询'], headers=headers)
+    print(student_grade_query.text)
+    soup = BeautifulSoup(student_grade_query.text, 'lxml')
+    __VIEWSTATE = soup.find_all('input', type="hidden")[0]['value']
+    query_post_data = {
+        '__VIEWSTATE': __VIEWSTATE,
+        'ddlXN':'',
+        'ddlXQ': '1',
+        'Button1': ''
+    }
+    headers['Referer'] = link_dic['成绩查询']
+    user_grade = session.post(link_dic['成绩查询'], data=query_post_data, headers=headers)
+    print('user_grade',user_grade.text)
+
